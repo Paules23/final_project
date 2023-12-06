9786455c44da9970622a6d1fb8cc35c90c9ddd07
Paules <pauvz123@gmail.com>
2023-12-06 12:54:07 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:final_project/screens/home_screen.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tu Aplicación',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+      // routes: {
+      //   '/home': (context) => HomeScreen(),
+      //
+      // },
+    );
+  }
+}
